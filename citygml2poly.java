fbe82410ed03a1d6c3d51a51498efdc359aa5c92
h.ledoux@tudelft.nl <h.ledoux@tudelft.nl@0e5b430a-d884-acd6-0e4a-89e9216abef7>
2012-05-30 11:03:06 +0000

    ignoring files + main takes 2 arguments

--- citygml2poly.java
+++ citygml2poly.java
@@ -4,18 +4,20 @@ import java.io.File;
 class citygml2poly {
   public static void main(String[] args) {
     
-    // if (args.length != 2) {
-    //   System.out.println("\nUsage: java -jar citygml2poly.jar [infile] [outfolder]\n");
-    // }
-    // else {
-      // VReaderWriter readerWriter = new VReaderWriter(new File(args[0]), new File(args[1]));
-      VReaderWriter readerWriter = new VReaderWriter(new File("/Users/hugo/data/citygml/CityGML_British_Ordnance_Survey_v1.0.0.xml"), new File("/Users/hugo/temp/y/"));
+    if (args.length != 2) {
+      System.out.println("\nUsage: citygml2poly [infile] [outfolder]\n");
+    }
+    else {
+      System.out.println(args[0]);
+      VReaderWriter readerWriter = new VReaderWriter(new File(args[0]), new File(args[1]));
+
+      // VReaderWriter readerWriter = new VReaderWriter(new File("/Users/hugo/data/citygml/CityGML_British_Ordnance_Survey_v1.0.0.xml"), new File("/Users/hugo/temp/y/"));
       try { 
         readerWriter.organizeConversion();
       }
       catch (Exception e) {
         e.printStackTrace();
       }
-    // }
+    }
   }
 }

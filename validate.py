3297ca027c65a7a9c65ce1f3d614455c71830c71
h.ledoux@tudelft.nl <h.ledoux@tudelft.nl@0e5b430a-d884-acd6-0e4a-89e9216abef7>
2012-05-31 07:28:33 +0000

    now working even better, with multiple shells per solid. Left: glueing together the XML returns of the solids and put the solid name

--- validate.py
+++ validate.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-untitled.py
 
-Created by Hugo Ledoux on 2012-05-30.
-Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-"""
 
 import sys
 import os
@@ -19,7 +14,6 @@ def main():
 
 # 1. create and/or clear the tmp folder
   if not os.path.exists("tmp"):
-    print "create new one"
     os.mkdir("tmp")
   else:
     shutil.rmtree("tmp")
@@ -35,21 +29,28 @@ def main():
 
 # 3. validate each building/shell
   os.chdir('tmp')
-  # print len(glob.glob('*.poly'))
-  lsFiles =[]
+  dFiles = {}
   for f in os.listdir('.'):
-    lsFiles.append(f)
-  print len(lsFiles)
+    if f[-4:] == 'poly':
+      i = (f.split('.poly')[0]).rfind('.')
+      f1 = f[:i]
+      if f1 not in dFiles:
+        dFiles[f1] = [f]
+      else:
+        dFiles[f1].append(f)
   val3dity = '/Users/hugo/Library/Developer/Xcode/DerivedData/val3dity-btcvseqwbnkwbueknlulczqmjyqt/Build/Products/Debug/val3dity'
-
   i = 0
-  for f in lsFiles:
-    strr = val3dity + " " + f
-    print strr
-    os.system(val3dity + " -withids -xml " + f)
+  for solidname in dFiles:
+    str1 = val3dity + " -withids -xml " +  " ".join(dFiles[solidname])
+    print str1
+    os.system(str1)
     i += 1
     if i == 3:
-      sys.exit()
+      break
+
+# 4. wipe the tmp folder
+  os.chdir('../')
+  shutil.rmtree('tmp')
 
 
 if __name__ == '__main__':
